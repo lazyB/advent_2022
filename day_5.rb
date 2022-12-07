aad311f0affa71213217bbff5a4cf841fc3e7e93
Will Archer <warcher@lolofit.com>
2022-12-06 21:19:27 -0700

    day 5 complete

--- day_5.rb
+++ day_5.rb
@@ -15,3 +15,24 @@ stacks = (1..9).reduce({}) {|stacks, i| stacks[i] = []; stacks}
   end
   9.times {|i| puts "#{i + 1} #{stacks[i+ 1]}"}
 end
+
+2.times {f.readline }
+
+while !f.eof? && line = f.readline do
+  tokens = line.split
+  count = tokens[1].to_i
+  source = tokens[3].to_i
+  dest = tokens[5].to_i
+  source_stack = stacks[source]
+  dest_stack = stacks[dest]
+  if count > source_stack.size
+    puts "wut?"
+  end
+  dest_stack << source_stack.slice!(source_stack.size - count, count)
+  dest_stack.flatten!
+end
+
+
+(1..9).each {|i|
+  puts stacks[i].last
+}
