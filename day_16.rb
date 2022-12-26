2d7a1e3d8e46932893cc2b6308a1add2cda5e053
Will Archer <warcher@lolofit.com>
2022-12-26 13:59:15 -0700

    day 16 WIP

--- day_16.rb
+++ day_16.rb
@@ -1,4 +1,5 @@
 MAX_TIME = 30
+PRINT_ENABLED = ENV['PRINT_ENABLED'] || false
 
 class Map
   attr_accessor :valves
@@ -23,6 +24,7 @@ class Map
       end
       valve.edges[edge_name] = edge
     end
+    valve
   end
 end
 
@@ -36,7 +38,137 @@ class Valve
 end
 
 map = Map.new
+class TravelState
+  attr_accessor :current_valve, :traveled_valve, :time_elapsed, :total_flow_rate, :open_valves,
+                :total_flow_amount, :history
 
+  @@cache_hits = 0
+  @@cache_misses = 0
+
+  def self.cache_hits
+    @@cache_hits
+  end
+
+  def self.cache_misses
+    @@cache_misses
+  end
+
+  def initialize(current_valve)
+    self.current_valve = current_valve
+    @open_valves = []
+    @time_elapsed = 0
+    @traveled_valve = [current_valve]
+    @history = [current_valve]
+    @total_flow_amount = 0
+    @total_flow_rate = 0
+  end
+
+  def remaining_time
+    MAX_TIME - time_elapsed
+  end
+
+  def avail_edges
+    sorted = self.current_valve.edges.values.sort{|v1, v2| v2.flow <=> v1.flow}
+    new_edges = sorted.reject {|edge| self.traveled_valve.include? edge }
+    all_edges = sorted - new_edges
+    new_edges + all_edges
+  end
+
+  def open_valve
+    new_state = self.clone
+    new_state.time_elapsed += 1
+    new_state.total_flow_amount += new_state.total_flow_rate
+    new_state.total_flow_rate += self.current_valve.flow
+    new_state.open_valves = self.open_valves.clone
+    new_state.open_valves << self.current_valve
+    new_state.traveled_valve = self.traveled_valve.clone
+    new_state
+  end
+
+  def travel_edge(edge)
+    new_state = self.clone
+    new_state.total_flow_amount = self.total_flow_amount + self.total_flow_rate
+    new_state.time_elapsed = self.time_elapsed + 1
+    new_state.current_valve = edge
+    new_state.traveled_valve = @traveled_valve.clone
+    new_state.traveled_valve << edge
+    new_state.history = @history.clone
+    new_state.history << edge
+    new_state
+  end
+
+  def open_valve_names
+    @open_valves.map(&:name).sort.join(",")
+  end
+
+  def print_state(force_print = false)
+    return unless PRINT_ENABLED || force_print
+    puts "time: #{@time_elapsed} rate: #{@total_flow_rate} amount:#{@total_flow_amount} open:#{open_valve_names}"
+    puts "history: #{@history.map(&:name).join("->") }"
+  end
+
+  def proj_total
+    @total_flow_amount + @total_flow_rate * remaining_time
+  end
+
+  def spider
+    if self.time_elapsed == MAX_TIME
+      puts "REACHED END" if PRINT_ENABLED
+      print_state
+      return self
+    elsif fetch_memo(self)
+      @@cache_hits += 1
+      new_state = self.clone
+      new_state.total_flow_amount += fetch_memo(self)
+      return new_state
+    else
+      @@cache_misses += 1
+    end
+
+    best_state = self
+    if !open_valves.include?(self.current_valve) && @current_valve.flow > 0
+      # open valve somehow
+      new_state_open = open_valve
+      puts"OPEN VALVE #{@current_valve.name}" if PRINT_ENABLED
+      new_state_open.print_state
+      best_state = new_state_open.spider
+    end
+    avail_edges.each {|e|
+      if @current_valve.name == "AA"
+        puts "check A" if PRINT_ENABLED
+      end
+      puts "from #{@current_valve.name} travel edge #{e.name}" if PRINT_ENABLED
+      new_map = travel_edge(e)
+      new_state = new_map.spider
+      puts "spider state:" if PRINT_ENABLED
+      new_state.print_state
+      if new_state.total_flow_amount > best_state.total_flow_amount
+        puts "NEW BEST STATE" if PRINT_ENABLED
+        new_state.print_state
+        best_state = new_state
+      end
+    }
+    memoize(best_state, best_state.total_flow_amount - @total_flow_amount)
+    return best_state
+  end
+
+
+  @@state_memo ||= {}
+
+  def memoize(state, delta)
+    @@state_memo[state.current_valve.name] ||= {}
+    @@state_memo[state.current_valve.name][state.open_valve_names] ||= {}
+    @@state_memo[state.current_valve.name][state.open_valve_names][remaining_time] = delta
+  end
+
+  def fetch_memo(state)
+    return nil unless @@state_memo&.[](state.current_valve.name)&.[](state.open_valve_names)
+    @@state_memo[state.current_valve.name][state.open_valve_names][remaining_time]
+  end
+
+end
+
+first_valve = nil
 File.open("day_16.input").each_line do |line|
   raw_flow, raw_edges = line.split ';'
   flow_parts = raw_flow.split
@@ -44,57 +176,15 @@ File.open("day_16.input").each_line do |line|
   flow = flow_parts.last.split('=').last.to_i
   _, _, _, _, *rest = raw_edges.split
   edges = rest.map {|raw| raw.delete (',')}
-  map.add_valve(name, flow, edges)
+  valve = map.add_valve(name, flow, edges)
+  first_valve = valve if first_valve.nil?
 end
 
 puts map
 
-class TravelState
-  attr_accessor :current_valve, :traveled_valve, :time_elapsed, :total_flow_rate, :open_valves,
-                :total_flow_amount
+initial_state = TravelState.new(first_valve)
 
-  def initialize(current_valve)
-    self.current_valve = current_valve
-    super
-  end
+answer = initial_state.spider
 
-  def avail_edges
-    new_edges = self.current_valve.edges.reject {|edge| self.traveled_valve.contains? edge }
-    all_edges = self.current_valve.edges - new_edges
-    new_edges + all_edges
-  end
-
-  def open_valve
-    new_map = self.clone
-    new_map.time_elapsed += 1
-    new_map.total_flow_amount += new_map.total_flow_rate
-    new_map.total_flow_rate += self.current_valve.flow
-    new_map.open_valves = self.open_valves.clone
-    new_map.open_valves << self.current_valve
-    new_map.traveled_valve = self.traveled_valve.clone
-  end
-
-  def travel_edge(edge)
-    new_flow = self.total_flow_amount + self.total_flow_rate
-    new_time = self.time_elapsed + 1
-    new_map = self.clone
-    new_map.total_flow_amount = new_flow
-    new_map.time_elapsed = new_time
-    new_map.current_valve = edge
-    new_map.traveled_valve << edge
-    new_map
-  end
-
-  def spider
-    return self.total_flow_amount if self.time_elapsed == MAX_TIME
-
-    if !open_valves.include? self.current_valve
-      # open valve somehow
-    else
-      avail_edges.each {|e|
-        travel_edge(e)
-
-}
-    end
-  end
-end
+puts("FINAL ANSWER hits: #{TravelState.cache_hits} misses: #{TravelState.cache_misses}")
+answer.print_state
